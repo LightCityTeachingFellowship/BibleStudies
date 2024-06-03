34e7d86addfede9edef31ac396289cb3c61a5f1d
SammyLight <59570031+SammyLight@users.noreply.github.com>
2024-06-03 19:22:59 +0100

    Add files via upload

--- script/victorScripts/videos.js
+++ script/victorScripts/videos.js
@@ -104,7 +104,7 @@ function addArrowIndicatorLeft() {
     arrowIndicatorLeft.innerHTML = `
         <div id="arrow-left-btn" style="fill: currentcolor;">
             <div class="arrow-btn-size">
-                <div style="width: 100%; height: 100%; fill: currentcolor;">
+                <div style="width: 150%; height: 100%; fill: currentcolor;">
                     <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24" focusable="false" style="pointer-events: none; display: block; width: 100%; height: 100%;">
                         <path d="M14.96 18.96 8 12l6.96-6.96.71.71L9.41 12l6.25 6.25-.7.71z"></path>
                     </svg>
@@ -126,7 +126,7 @@ function addArrowIndicatorRight() {
     arrowIndicatorRight.innerHTML = `
         <div id="arrow-right-btn" style="fill: currentcolor;">
             <div class="arrow-btn-size">
-                <div style="width: 100%; height: 100%; fill: currentcolor;">
+                <div style="width: 150%; height: 100%; fill: currentcolor;">
                     <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24" focusable="false" style="pointer-events: none; display: block; width: 100%; height: 100%;">
                         <path d="m9.4 18.4-.7-.7 5.6-5.6-5.7-5.7.7-.7 6.4 6.4-6.3 6.3z"></path>
                     </svg>
