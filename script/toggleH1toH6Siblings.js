39e6d098b2cd75b751b6e8ae9f320053f462ca35
SamAwo <55980014+SamuelAwojuola@users.noreply.github.com>
2025-12-30 14:54:16 +0100

    Update toggleH1toH6Siblings.js ::: check `H1to6` not in `header`

--- script/toggleH1toH6Siblings.js
+++ script/toggleH1toH6Siblings.js
@@ -74,7 +74,7 @@ if(!document.querySelector('#homeBodyContent')){
     // AutoAdjust checked state of #toggleAllBtn
     const toggleAllBtn = document.body.querySelector('#toggleAllBtn');
     const article = document.body.querySelector('article');
-    document.body.querySelectorAll('article :is(h6,h5,h4,h3,h2,h1)').forEach(h=>{
+    document.body.querySelectorAll('article :is(h6,h5,h4,h3,h2,h1):not(header > :is(h6,h5,h4,h3,h2,h1))').forEach(h=>{
         h.addEventListener('click', ()=>{
             if(!article.querySelector('.hidingsibs')){
                 // All headers are showing lower siblings
@@ -281,6 +281,7 @@ function closestScrollableAncestors(element,limit) {
     }
     return { elm:scrollableAncestor, x: scrollableAncestorX, y: scrollableAncestorY };
 }
+
 
 
 
